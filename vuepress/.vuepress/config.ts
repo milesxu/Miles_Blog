404b104a39af30c4e8f4652688e2f80c2e2f4be0
Miles <milesxu@outlook.com>
2022-01-04 15:51:14 +0800

    some plan added.

---
+++ vuepress/.vuepress/config.ts
@@ -0,0 +1,15 @@
+import { defineUserConfig } from 'vuepress';
+import type { DefaultThemeOptions } from 'vuepress';
+import theme from 'vuepress-theme-hope';
+
+export default theme.config({});
+
+/*export default defineUserConfig<DefaultThemeOptions>({
+  lang: 'en-US',
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+  },
+});*/
